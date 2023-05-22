1ba7d20f12caa8375cf8705f54634abe5b8a4558
tuva9 <tuva9@fpt.edu.vn>
2023-05-22 22:58:17 +0700

    update

--- before/app.js
+++ before/app.js
@@ -4,4 +4,4 @@ window.onscroll = function () {
     const percentage = scroll / height * 100
     document.querySelector("#myBar").style.width = percentage + "%"
 }
-// Edit sth 2
+// Edit sth tuva9_3
