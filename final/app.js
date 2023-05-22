6bb6e119e9b59c3ad97c4f90e60afb0b0b6ca961
tuva9 <tuva9@fpt.edu.vn>
2023-05-22 21:42:05 +0700

    Delete js file

--- final/app.js
+++
@@ -1,7 +0,0 @@
-window.onscroll = function () {
-    var windowScroll = document.documentElement.scrollTop
-    console.log(windowScroll);
-    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight
-    var percentage = windowScroll / height * 100
-    document.querySelector("#myBar").style.width = percentage + "%"
-}
